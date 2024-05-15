d38d54573a238562f5f664e4a664c530bf8cb8cb
Carissa Hluchan <carissahluchan@gmail.com>
2024-05-15 08:58:59 -0600

    Add functions to dragons and birthday

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,25 @@
+function createBirthday(name, month, day) {
+    var birthday = {
+        name: name,
+        month: month,
+        day: day,
+    }
+    return birthday;
+}
+// what does it take in - 3 paramerters a name, month, day
+// does it return anything - return the object
+// what is it's purpose - creates a lsit of objects
 
 
-module.exports = {  };
+
+
+// what does it take in
+// does it return anything
+// what is it's purpose
+
+
+
+
+module.exports = {
+    createBirthday,
+  };
--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,59 @@
 
 
+function createDragon (dragonName, dragonRider, dragonTemperment) {
+  var dragon = {
+    name: dragonName,
+    rider: dragonRider,
+    temperment: dragonTemperment,
+    timesEaten: 0,
+    hungry: true,
+  }
+  return dragon
+}
+
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false;
+  }
+// what does it take in - dragon object
+// does it return anything - a dragon object
+// what is it's purpose - times eatten incrament up
+// checks hungry key and at 3 updates hungry is false
+return dragon;
+}
+
+function findFireBreathers(arrOfDragons) {
+  // console.log('Array of Dragons', arrOfDragons)
+  var fireBreathers = [];
+  for (var i = 0; i < arrOfDragons.length; i++) {
+    if (arrOfDragons[i].temperment === "aggressive") {
+      fireBreathers.push(arrOfDragons[i]);
+    }
+  };
+
+// what does it take in - an array of dragon objects
+// does it return anything - an array of dragon objects
+// what is it's purpose - find all dragons that are aggressive 
+// assign them firebreathers return an array of all the firebreathers
+return fireBreathers;
+}
+
+
+
+// what does it take in
+// does it return anything
+// what is it's purpose
+
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
