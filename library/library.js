61cb77648b53336144336f50a61a2883ef90fbe0
Carissa Hluchan <carissahluchan@gmail.com>
2024-06-04 09:16:04 -0600

    finishes library

--- library/library.js
+++ library/library.js
@@ -1,3 +1,30 @@
+function issueCard(name, age, numBooksCheckedOut) {
+    var card = {
+        name: name,
+        age: age,
+        numBooksCheckedOut: numBooksCheckedOut || 0,
+        isChild: false 
+    }
+    if (age < 12) {
+        card.isChild = true;
+    }
+    return card;
+}
 
+function searchByAuthor(collection, author) {
+    collectionByAuthor = [];
+    for (var i = 0; i < collection.length; i++) {
+        if (collection[i].author === author) {
+            collectionByAuthor.push(collection[i])
+        }
+    }
+    if (!collectionByAuthor.length) {
+        return `No book found for search criteria`;
+    }
+    return collectionByAuthor
+}
 
-module.exports = { };
+module.exports = {
+    issueCard,
+    searchByAuthor,
+ };
