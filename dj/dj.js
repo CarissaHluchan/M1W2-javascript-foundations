f32328dc49343a194a72847dd1941a62caa5a666
Carissa Hluchan <carissahluchan@gmail.com>
2024-06-04 15:24:31 -0600

    Finish DJ and hobbits

--- dj/dj.js
+++ dj/dj.js
@@ -1,8 +1,53 @@
+function createSong(name, artist, favorite) {
+  var song = {
+    name: name,
+    artist: artist,
+    favorite: favorite || false,
+    hasBeenPlayed: false,
+  }
+  return song
+}
+
+function playSong(song) {
+  if(!song.hasBeenPlayed) {
+    song.hasBeenPlayed = true;
+  }
+  return song;
+}
+
+function makePlaylist(name, songs) {
+  var playlist = {
+    name: name,
+    songs: songs
+  }
+  return playlist;
+}
+
+function addSongToPlaylist(playlist, song) {
+  playlist.songs.push(song);
+  return playlist
+}
+
+function playSongs(playlist) {
+  if (arguments[1]) {
+    for (var i = 0; i < playlist.songs.length; i++) {
+      if (playlist.songs[i].favorite) {
+      playlist.songs[i].hasBeenPlayed = true;
+      }
+    }
+  } else {
+    for (var i = 0; i < playlist.songs.length; i++) {
+      playlist.songs[i].hasBeenPlayed = true;
+    }
+  }
+  return playlist;
+}
+
 
 module.exports = { 
-  // createSong, 
-  // playSong, 
-  // makePlaylist, 
-  // addSongToPlaylist, 
-  // playSongs
+  createSong, 
+  playSong, 
+  makePlaylist, 
+  addSongToPlaylist, 
+  playSongs
 };
